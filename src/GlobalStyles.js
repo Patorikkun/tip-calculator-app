2cb03d6fc23825fc3927d00dda8ad4f88ed55ea4
Patorikkun <63767819+Patorikkun@users.noreply.github.com>
2021-08-04 12:39:12 +0800

    relocated-imports

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -2,10 +2,10 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
 
-
+@import url("https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap");
 
 * {
-    @import url("https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap");
+  
     margin: 0;
     padding: 0;
     box-sizing: border-box;
